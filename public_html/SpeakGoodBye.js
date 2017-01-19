07f38f9db887556ba4811668ca2a6bc639e943a2
Ramon Chiara <ramonchiara@gmail.com>
2017-01-19 00:59:56 -0200

    Step 9.

--- public_html/SpeakGoodBye.js
+++ public_html/SpeakGoodBye.js
@@ -24,4 +24,5 @@
     // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
     // 'byeSpeaker' on the global scope as well.
     // xxxx.xxxx = byeSpeaker;
+    window.byeSpeaker = byeSpeaker;
 })(window);
