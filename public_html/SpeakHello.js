0ba2928d011e6074977ad5fdc597541a383da9e7
Ramon Chiara <ramonchiara@gmail.com>
2017-01-19 00:57:23 -0200

    Step 5.

--- public_html/SpeakHello.js
+++ public_html/SpeakHello.js
@@ -23,4 +23,5 @@
     // See Lecture 52, part 2
     // (Note, Step 6 will be done in the SpeakGoodBye.js file.)
     // xxxx.xxxx = helloSpeaker;
+    window.helloSpeaker = helloSpeaker;
 })(window);
